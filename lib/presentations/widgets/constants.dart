37da44dd8d6b39efc6404589dab788b6962d5189
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-01-31 21:48:24 +0530

    height5 added to constants

--- lib/presentations/widgets/constants.dart
+++ lib/presentations/widgets/constants.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/widgets.dart';
 
-class Constants{
-  static const Widget width8= SizedBox(
+class Constants {
+  // Width Constants
+  static const Widget width8 = SizedBox(
     width: 8,
+  );
+
+  // Height Constants
+  static const Widget height5 = SizedBox(
+    height: 5,
   );
 }
