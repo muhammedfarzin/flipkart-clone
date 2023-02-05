39425cc8f682e087b65d70fd8f6192e5363f32ee
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-05 16:13:06 +0530

    Class Dummy

---
+++ lib/presentations/widgets/dummy_data.dart
@@ -0,0 +1,8 @@
+class DummyData{
+  //Offers
+  static const List<String> topOffers = [
+    "https://static-ai.asianetnews.com/images/01g5ecxm6pnmd19398xp4k7fhj/whatsapp-image-2022-06-13-at-4-49-15-pm--3-.jpg",
+    "https://rukminim1.flixcart.com/image/612/612/ktizdzk0/shoe/y/b/x/7-ws-9310-tying-grey-original-imag6ut3hzm2zyqm.jpeg?q=70",
+    "https://www.shopyvision.com/wp-content/uploads/2022/01/Fire-Boltt-360-Pro-Bluetooth-Calling-Smart-Watch-2-min.jpg",
+  ];
+}
