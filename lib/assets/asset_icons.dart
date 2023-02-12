63f7a57295b2fe9e57f3ec3faed438706c96d1f8
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-12 19:02:40 +0530

    Custom Asset Icons created

---
+++ lib/assets/asset_icons.dart
@@ -0,0 +1,8 @@
+class AssetIcons {
+  static const card = "assets/icons/card.png";
+  static const gift = "assets/icons/gift.png";
+  static const headset = "assets/icons/headset.png";
+  static const heart = "assets/icons/heart.png";
+  static const package = "assets/icons/package.png";
+  static const payLater = "assets/icons/payLater.png";
+}
