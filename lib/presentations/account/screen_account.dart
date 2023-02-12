1484a2510b2ab65adceab33eba2b81e23e0d8851
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-12 23:42:58 +0530

    ScreenAccount background to grey

--- lib/presentations/account/screen_account.dart
+++ lib/presentations/account/screen_account.dart
@@ -9,149 +9,165 @@ class ScreenAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       body: SafeArea(
         child: ListView(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 15.0,
-                right: 15.0,
-                bottom: 8,
-                left: 15.0,
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 2,
+                  ),
+                ),
               ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Column(
                 children: [
-                  // Salutation and Flipkart Plus
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10.0, top: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // Salutation
-                          const Text(
-                            "Hey, Username",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 15.0,
+                      right: 15.0,
+                      bottom: 8,
+                      left: 15.0,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Salutation and Flipkart Plus
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10.0, top: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Salutation
+                                const Text(
+                                  "Hey, Username",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                                Constants.height10,
+                                Row(
+                                  children: [
+                                    Text(
+                                      "Join",
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.grey.shade600,
+                                      ),
+                                    ),
+                                    Text(
+                                      " Flipkart",
+                                      style: TextStyle(
+                                        color: Constants.customDeepBlue,
+                                        fontStyle: FontStyle.italic,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    Text(
+                                      " Plus",
+                                      style: TextStyle(
+                                        color: Colors.yellow.shade700,
+                                        fontStyle: FontStyle.italic,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    const Icon(
+                                      Icons.keyboard_arrow_right,
+                                      size: 17,
+                                    )
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
-                          Constants.height10,
-                          Row(
+                        ),
+
+                        // SuperCoin
+                        Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 1,
+                                color: Colors.grey.shade300,
+                              ),
+                              borderRadius: BorderRadius.circular(50)),
+                          padding: const EdgeInsets.all(4.0),
+                          child: Wrap(
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            direction: Axis.horizontal,
                             children: [
-                              Text(
-                                "Join",
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  color: Colors.grey.shade600,
+                              CircleAvatar(
+                                backgroundColor: Colors.yellow.shade700,
+                                radius: 10,
+                                child: const Icon(
+                                  Icons.electric_bolt,
+                                  color: Colors.white,
+                                  size: 14,
                                 ),
                               ),
-                              Text(
-                                " Flipkart",
+                              Constants.width3,
+                              const Text(
+                                "155",
                                 style: TextStyle(
-                                  color: Constants.customDeepBlue,
-                                  fontStyle: FontStyle.italic,
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 15,
                                 ),
                               ),
-                              Text(
-                                " Plus",
-                                style: TextStyle(
-                                  color: Colors.yellow.shade700,
-                                  fontStyle: FontStyle.italic,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                ),
-                              ),
-                              const Icon(
-                                Icons.keyboard_arrow_right,
-                                size: 17,
-                              )
                             ],
                           ),
-                        ],
-                      ),
+                        )
+                      ],
                     ),
                   ),
+                  Constants.lineH1,
 
-                  // SuperCoin
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1,
-                          color: Colors.grey.shade300,
-                        ),
-                        borderRadius: BorderRadius.circular(50)),
-                    padding: const EdgeInsets.all(4.0),
-                    child: Wrap(
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      direction: Axis.horizontal,
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Colors.yellow.shade700,
-                          radius: 10,
-                          child: const Icon(
-                            Icons.electric_bolt,
-                            color: Colors.white,
-                            size: 14,
+                  // Buttons Like Orders and Wishlist
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      children: const [
+                        Flexible(
+                          child: IconLabelButtonOutlined(
+                            icon: AssetIcons.package,
+                            label: "Orders",
                           ),
                         ),
-                        Constants.width3,
-                        const Text(
-                          "155",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                        Flexible(
+                          child: IconLabelButtonOutlined(
+                            icon: AssetIcons.heart,
+                            label: "Wishlist",
                           ),
                         ),
                       ],
                     ),
-                  )
-                ],
-              ),
-            ),
-            Constants.lineH1,
-
-            // Buttons Like Orders and Wishlist
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                children: const [
-                  Flexible(
-                    child: IconLabelButtonOutlined(
-                      icon: AssetIcons.package,
-                      label: "Orders",
-                    ),
                   ),
-                  Flexible(
-                    child: IconLabelButtonOutlined(
-                      icon: AssetIcons.heart,
-                      label: "Wishlist",
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      children: const [
+                        Flexible(
+                          child: IconLabelButtonOutlined(
+                            icon: AssetIcons.gift,
+                            label: "Coupons",
+                          ),
+                        ),
+                        Flexible(
+                          child: IconLabelButtonOutlined(
+                            icon: AssetIcons.headset,
+                            label: "Help Center",
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                children: const [
-                  Flexible(
-                    child: IconLabelButtonOutlined(
-                      icon: AssetIcons.gift,
-                      label: "Coupons",
-                    ),
-                  ),
-                  Flexible(
-                    child: IconLabelButtonOutlined(
-                      icon: AssetIcons.headset,
-                      label: "Help Center",
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            )
           ],
         ),
       ),
