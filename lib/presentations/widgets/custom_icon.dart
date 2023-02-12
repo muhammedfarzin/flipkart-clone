ecf9ef98342f14f3f885ab1290241daafd667989
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-12 19:03:12 +0530

    CustomIcon Widget created

---
+++ lib/presentations/widgets/custom_icon.dart
@@ -0,0 +1,25 @@
+import 'package:flipkart/assets/asset_icons.dart';
+import 'package:flipkart/presentations/widgets/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({
+    super.key,
+    required this.icon,
+    this.color,
+    this.scale = 25,
+  });
+
+  final String icon;
+  final Color? color;
+  final double scale;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      icon,
+      color: color ?? Constants.customDeepBlue,
+      scale: scale,
+    );
+  }
+}
