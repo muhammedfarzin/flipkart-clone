9a3b625116dda6c5b4c13c6903cb72c698ef4935
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-13 20:43:52 +0530

    Some new AssetIcons added

--- lib/assets/asset_icons.dart
+++ lib/assets/asset_icons.dart
@@ -5,4 +5,13 @@ class AssetIcons {
   static const heart = "assets/icons/heart.png";
   static const package = "assets/icons/package.png";
   static const payLater = "assets/icons/pay-later.png";
+  static const chat = "assets/icons/chat.png";
+  static const editing = "assets/icons/editing.png";
+  static const languages = "assets/icons/languages.png";
+  static const location = "assets/icons/location.png";
+  static const notificationSettings = "assets/icons/notification-settings.png";
+  static const shop = "assets/icons/shop.png";
+  static const star = "assets/icons/star.png";
+  static const user = "assets/icons/user.png";
+  static const wallet = "assets/icons/wallet.png";
 }
