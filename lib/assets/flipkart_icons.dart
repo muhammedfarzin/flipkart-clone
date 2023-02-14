8a43b5a9d9e7475869cd436dfd728c79bd886bca
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-14 23:50:51 +0530

    FlipkartIcons created and flipkart assets added

---
+++ lib/assets/flipkart_icons.dart
@@ -0,0 +1,16 @@
+class FlipkartIcons {
+  static const camera = "assets/flipkart_icons/camera.png";
+  static const coupons = "assets/flipkart_icons/coupons.png";
+  static const creditCard = "assets/flipkart_icons/credit-card.png";
+  static const fireDrops = "assets/flipkart_icons/fire-drops.png";
+  static const flipkartBanner = "assets/flipkart_icons/flipkart-banner.png";
+  static const games = "assets/flipkart_icons/games.png";
+  static const groceryBanner = "assets/flipkart_icons/grocery-banner.png";
+  static const groupBuy = "assets/flipkart_icons/group-buy.png";
+  static const liveShop = "assets/flipkart_icons/live-shop.png";
+  static const plus = "assets/flipkart_icons/plus.png";
+  static const sellerBack = "assets/flipkart_icons/seller-back.png";
+  static const sellerHub = "assets/flipkart_icons/seller-hub.png";
+  static const superCoin = "assets/flipkart_icons/super-coin.png";
+  static const whatsNew = "assets/flipkart_icons/whats-new.png";
+}
