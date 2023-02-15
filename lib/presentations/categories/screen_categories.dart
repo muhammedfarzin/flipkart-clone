cd5e4e80b4f2761fd574bc6181301de5c51c0900
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-15 22:25:53 +0530

    ScreenCategories AppBar Created

--- lib/presentations/categories/screen_categories.dart
+++ lib/presentations/categories/screen_categories.dart
@@ -1,14 +1,48 @@
+import "package:flipkart/presentations/widgets/constants.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
 class ScreenCategories extends StatelessWidget {
   const ScreenCategories({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text("Screen Category"),
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.blue.shade700,
+      ),
+      child: Scaffold(
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: [
+              // AppBar
+              SliverAppBar(
+                // AppBar Background Color
+                backgroundColor: Constants.customBlue,
+
+                // Title
+                title: const Text(
+                  "All Categories",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+
+                // Actions
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.mic),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
