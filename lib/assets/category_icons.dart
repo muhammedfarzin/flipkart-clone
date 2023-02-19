bd54dbc68a67199a289bb446591b48ef6f3d61c1
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-19 20:55:59 +0530

    Some Category Icons Added

---
+++ lib/assets/category_icons.dart
@@ -0,0 +1,26 @@
+class CategoryIcons {
+  static const appliances = "assets/category_icons/appliances.png";
+  static const bikesAndCars =
+      "assets/category_icons/bikes-and-cars.png";
+  static const electronics = "assets/category_icons/electronics.png";
+  static const fasion = "assets/category_icons/fasion.png";
+  static const flightsAndHotels =
+      "assets/category_icons/flights-and-hotels.png";
+  static const furniture = "assets/category_icons/furniture.png";
+  static const giftCards = "assets/category_icons/gift-cards.png";
+  static const grocery = "assets/category_icons/grocery.png";
+  static const homeItems = "assets/category_icons/home-items.png";
+  static const homeService =
+      "assets/category_icons/home-service.png";
+  static const insurance = "assets/category_icons/insurance.png";
+  static const medicines = "assets/category_icons/medicines.png";
+  static const mobiles = "assets/category_icons/mobiles.png";
+  static const nutrition = "assets/category_icons/nutrition.png";
+  static const offerZone = "assets/category_icons/offer-zone.png";
+  static const personalCare =
+      "assets/category_icons/personal-care.png";
+  static const sellBack = "assets/category_icons/sell-back.png";
+  static const sports = "assets/category_icons/sports.png";
+  static const toysAndBaby =
+      "assets/category_icons/toys-and-baby.png";
+}
