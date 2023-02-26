144b10792db0929a621d0ba5a0fda8ce61f89a3b
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-02-26 16:10:14 +0530

    Trending Stores asset added

---
+++ lib/assets/trending_stores_icons.dart
@@ -0,0 +1,12 @@
+class TrendingStoresIcons {
+  static const flipkartEmergingBrands = "assets/trending_stores_icons/flipkart-emerging-brands.png";
+  static const flipkartGreen = "assets/trending_stores_icons/flipkart-green.png";
+  static const flipkartOriginals = "assets/trending_stores_icons/flipkart-originals.png";
+  static const flipkartSamarth = "assets/trending_stores_icons/flipkart-samarth.png";
+  static const happyWinter = "assets/trending_stores_icons/happy-winter.png";
+  static const internationalStore = "assets/trending_stores_icons/international-store.png";
+  static const launchHub = "assets/trending_stores_icons/launch-hub.png";
+  static const studentsClub = "assets/trending_stores_icons/students-club.png";
+  static const travelStore = "assets/trending_stores_icons/travel-store.png";
+  static const valentinesDay = "assets/trending_stores_icons/valentines-day.png";
+}
